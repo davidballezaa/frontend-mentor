1ea769ca01ee7470a8794639c812a99703c8d4ba
davidballezaa <davidballezaa@gmail.com>
2021-08-14 10:01:42 -0700

    added script to remove scroll when clicking hamburguer

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -2,6 +2,7 @@ const header = document.querySelector('.header');
 const hamburguerButton = document.getElementById('hamburguerButton');
 const overlay = document.querySelector('.overlay');
 const linksMobile = document.querySelector('.header__links--mobile');
+const body = document.querySelector('body');
 
 hamburguerButton.addEventListener('click', function(){
     if(header.classList.contains('open')){ //If it is closed
@@ -10,11 +11,13 @@ hamburguerButton.addEventListener('click', function(){
         overlay.classList.add('fade-out');
         linksMobile.classList.remove('fade-in');
         linksMobile.classList.add('fade-out');
+        body.classList.remove('no-scroll');
     } else {
         header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
         linksMobile.classList.remove('fade-out');
         linksMobile.classList.add('fade-in');
+        body.classList.add('no-scroll');
     }
 });
