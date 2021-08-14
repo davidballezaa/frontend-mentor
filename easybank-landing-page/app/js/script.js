32162bbc7f60e5ce8a908fd96bd8d99b1ae0973e
davidballezaa <davidballezaa@gmail.com>
2021-08-14 09:54:37 -0700

    finished with animation for the links in mobile when clicking hamburguer

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,15 +1,20 @@
 const header = document.querySelector('.header');
 const hamburguerButton = document.getElementById('hamburguerButton');
 const overlay = document.querySelector('.overlay');
+const linksMobile = document.querySelector('.header__links--mobile');
 
 hamburguerButton.addEventListener('click', function(){
     if(header.classList.contains('open')){ //If it is closed
         header.classList.remove('open');
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
+        linksMobile.classList.remove('fade-in');
+        linksMobile.classList.add('fade-out');
     } else {
         header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
+        linksMobile.classList.remove('fade-out');
+        linksMobile.classList.add('fade-in');
     }
 });
