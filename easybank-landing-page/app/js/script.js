99623356241c53c2fa39b515eced0c0e924438b2
davidballezaa <davidballezaa@gmail.com>
2021-08-16 19:05:50 -0700

    use prettier code formatter

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,23 +1,24 @@
-const header = document.querySelector('.header');
-const hamburguerButton = document.getElementById('hamburguerButton');
-const overlay = document.querySelector('.overlay');
-const linksMobile = document.querySelector('.header__links--mobile');
-const body = document.querySelector('body');
+const header = document.querySelector(".header");
+const hamburguerButton = document.getElementById("hamburguerButton");
+const overlay = document.querySelector(".overlay");
+const linksMobile = document.querySelector(".header__links--mobile");
+const body = document.querySelector("body");
 
-hamburguerButton.addEventListener('click', function(){
-    if(header.classList.contains('open')){ //If it is closed
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-        linksMobile.classList.remove('fade-in');
-        linksMobile.classList.add('fade-out');
-        body.classList.remove('no-scroll');
-    } else {
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-        linksMobile.classList.remove('fade-out');
-        linksMobile.classList.add('fade-in');
-        body.classList.add('no-scroll');
-    }
+hamburguerButton.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    //If it is closed
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+    linksMobile.classList.remove("fade-in");
+    linksMobile.classList.add("fade-out");
+    body.classList.remove("no-scroll");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+    linksMobile.classList.remove("fade-out");
+    linksMobile.classList.add("fade-in");
+    body.classList.add("no-scroll");
+  }
 });
